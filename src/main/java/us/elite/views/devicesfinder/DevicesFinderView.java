17893c5ed8f1e83c1e40870d04dfc1607fa4bb78
start.vaadin.com <no-reply@vaadin.com>
2023-01-18 13:23:42 +0000

    Generated project

---
+++ src/main/java/us/elite/views/devicesfinder/DevicesFinderView.java
@@ -0,0 +1,31 @@
+package us.elite.views.devicesfinder;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import us.elite.views.MainLayout;
+
+@PageTitle("Devices Finder")
+@Route(value = "devicesfinder", layout = MainLayout.class)
+public class DevicesFinderView extends VerticalLayout {
+
+    public DevicesFinderView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
---
+++ src/main/java/us/elite/views/portscanner/PortScannerView.java
@@ -0,0 +1,33 @@
+package us.elite.views.portscanner;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import us.elite.views.MainLayout;
+
+@PageTitle("Port Scanner")
+@Route(value = "portscanner", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class PortScannerView extends VerticalLayout {
+
+    public PortScannerView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
